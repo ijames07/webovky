87f221d73ea121e1405125be18bc6876acdb435d
Pavel Obrtlik <p.obrtlik@gmail.com>
2015-03-10 14:48:39 +0100

    -- rozpracovaný interface chatu -- jquery eventy

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
-
+	$('#snd').click(function() {
+		send();
+	});
+	$(document).keypress(function(e) {
+    if(e.which === 13) {
+        send();
+    }
+});
+function send() {
+	var msg = $('#msg').val();
+	if (msg === '') {
+		return;
+	}
+	$('#msg').val('');
+	var time = new Date();
+	$('#window').append('<p>[' + time.getHours() + ':' + ("0" + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2) + '] Adam: ' + msg + '</p>');
+}
 });
