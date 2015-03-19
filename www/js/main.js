3aa0411c4eb57aa59e81ad6032b2332a1f8fd75a
Pavel Obrtlik <p.obrtlik@gmail.com>
2015-03-19 23:58:01 +0100

    -- upraven jquery selektor pro vybrání správného inputu po kliknutí na vstoupit u zamknuté místnosti

--- www/js/main.js
+++ www/js/main.js
@@ -2,21 +2,21 @@ $(function(){
 	$('#send').click(function() {
 		send();
 	});
-	$(document).keypress(function(e) {
-    if(e.which === 13) {
-        send();
-    }
-});
-function send() {
-	var msg = $('#msg').val();
-	if (msg === '') {
-		return;
+	/*$(document).keypress(function(e) {
+		if(e.which === 13) {
+			send();
+		}
+	});*/
+	function send() {
+		var msg = $('#msg').val();
+		if (msg === '') {
+			return;
+		}
+		$('#msg').val('');
+		var time = new Date();
+		$('#window').append('<p>[' + time.getHours() + ':' + ("0" + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2) + '] Adam: ' + msg + '</p>');
 	}
-	$('#msg').val('');
-	var time = new Date();
-	$('#window').append('<p>[' + time.getHours() + ':' + ("0" + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2) + '] Adam: ' + msg + '</p>');
-}
-$('.modal').on('shown.bs.modal', function () {
-    $(this).find('input').focus();
-});
+	$('.modal').on('shown.bs.modal', function () {
+		$(this).find('[name=pw]').focus();
+	});
 });
