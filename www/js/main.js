772a0ea28d039a87ba9044d861817f61cd6183ef
Pavel Obrtlik <p.obrtlik@gmail.com>
2015-03-20 17:27:16 +0100

    -- upraven vzhled chatovací místnosti -- enter odesílá zprávy v chatu

--- www/js/main.js
+++ www/js/main.js
@@ -2,11 +2,11 @@ $(function(){
 	$('#send').click(function() {
 		send();
 	});
-	/*$(document).keypress(function(e) {
+	$(document).keypress(function(e) {
 		if(e.which === 13) {
 			send();
 		}
-	});*/
+	});
 	function send() {
 		var msg = $('#msg').val();
 		if (msg === '') {
@@ -14,7 +14,7 @@ $(function(){
 		}
 		$('#msg').val('');
 		var time = new Date();
-		$('#window').append('<p>[' + time.getHours() + ':' + ("0" + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2) + '] Adam: ' + msg + '</p>');
+		$('#window').prepend('<div class="thumbnail"><p>[' + time.getHours() + ':' + ("0" + time.getMinutes()).slice(-2) + ':' + ('0' + time.getSeconds()).slice(-2) + '] Adam: ' + msg + '</p></div>');
 	}
 	$('.modal').on('shown.bs.modal', function () {
 		$(this).find('[name=pw]').focus();
