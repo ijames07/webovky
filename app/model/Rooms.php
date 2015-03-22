120ed2156557ffccc3c5abb0fd37b603e65f9b9e
Pavel Obrtlik <p.obrtlik@gmail.com>
2015-03-22 17:30:44 +0100

    -- uživatelé vstupují a vystupují z místností

--- app/model/Rooms.php
+++ app/model/Rooms.php
@@ -22,4 +22,45 @@ class Rooms extends Nette\Object {
 	public function get($id = '') {
 		return $this->database->table('room')->get($id);
 	}
+	
+	/** @return int */
+	public function inRoom($user = '') {
+		if ($user == '') {
+			return;
+		}
+		return $this->database->table('in_chatroom')
+				->where('user_id', $user)
+				->fetch()->chatroom_id;
+	}
+	
+	/** @return int */
+	public function enterRoom($who, $where) {
+		$this->database->table('in_chatroom')
+				->where('user_id', $who)
+				->delete();
+		return $this->database->table('in_chatroom')
+				->insert(array(
+					'user_id'		=>	$who,
+					'chatroom_id'	=>	$where
+				));
+	}
+	
+	/** @return int */
+	public function updateLastMsg($id) {
+		return $this->database->table('in_chatroom')
+				->where('user_id', $id)
+				->update(array(
+					'last_message' => 'now()'
+				));
+	}
+	
+	/** @return Nette\Database\Table\Selection */
+	public function getRoomUsers($id = '') {
+		if ($id == '') {
+			return;
+		}
+		return $this->database->table('in_chatroom')
+				->where('chatroom_id', $id)
+				->order('last_message DESC');
+	}
 }
